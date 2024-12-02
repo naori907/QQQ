83203af0c3275e7756e7fee7ad9bdaccd547aa91
Amgalan <Amgalan@twst.com>
2024-12-02 22:05:05 +0800

    Ex0033

--- Ex003/obj/Debug/net8.0/Ex003.AssemblyInfo.cs
+++ Ex003/obj/Debug/net8.0/Ex003.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex003")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1343d1bf9b017dc288c58758931df8d474bbf414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d73f5fe8382bb86f46073e016a13cc2cd8572d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex003")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex003")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
